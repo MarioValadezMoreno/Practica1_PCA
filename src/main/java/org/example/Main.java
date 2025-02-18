c0b5f0b1f16c085ec7a8d45e49c01be42d0e91b2
Johan Mateo <mateo.pmunoz@alumnos.upm.es>
2025-02-18 19:52:45 +0100

    Comentarios

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,8 @@ public class Main
 {//prueba
     public static void main( String[] args ) {
         TesterPracticas TP = new TesterPracticas(new AlgoritmoBucketSort());
-        TP.evaluarPractica(TesterPracticas.Instancias.NUMBER_12500000, 6);
+        TP.evaluarPractica(TesterPracticas.Instancias.NUMBER_2500000, 6);
     }
+    //Tuvimos una tutoría con el profesor de prácticas, comprobamos que el código estaba bien, coincidía con lo que había que hacery ordenaba bien los números,
+    // pero los tiempos de ejecución eran muy largos, esto podía deberse al dispositivo (ordenador usado).
 }
