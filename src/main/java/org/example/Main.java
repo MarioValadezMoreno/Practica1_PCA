4daa5b9f8efe76575bd056a99eb0463ae7b4ea61
Mario <m.valadez@alumnos.upm.es>
2025-02-19 12:19:57 +0100

    AlgoritmoBucketSortExecutor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ import practicas_pca.TesterPracticas;
 public class Main
 {
     public static void main( String[] args ) {
-        TesterPracticas TP = new TesterPracticas(new AlgoritmoBucketSort());
-        TP.evaluarPractica(TesterPracticas.Instancias.NUMBER_2500000, 6);
+        TesterPracticas TP = new TesterPracticas(new AlgoritmoBucketSortExecutor());
+        TP.evaluarPractica(TesterPracticas.Instancias.NUMBER_25000000, 6);
     }
     //Tuvimos una tutoría con el profesor de prácticas, comprobamos que el código estaba bien, coincidía con lo que había que hacer y ordenaba bien los números,
     // pero los tiempos de ejecución eran muy largos, esto podía deberse al dispositivo (ordenador usado).
