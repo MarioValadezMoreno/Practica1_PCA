39f687017e784e8ae7fff5c269786e36e7d562ef
Mario <m.valadez@alumnos.upm.es>
2025-03-19 12:51:33 +0100

    p6 done

--- src/main/java/Practica5y6/MonitorP6.java
+++ src/main/java/Practica5y6/MonitorP6.java
@@ -13,15 +13,64 @@ public class MonitorP6 {
     private Condition hayPar = lock.newCondition();
     private Condition hayImpar = lock.newCondition();
 
-    public synchronized void addInt(Integer dato){
+    public void addInt(Integer dato){
+        lock.lock();
+        if(coleccion.size() >= MAX_ELEM){
+            try {
+                hayHueco.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        coleccion.add(dato); //Añade el dato a la coleccion
+        hayImpar.signal();
+        hayPar.signal();
+        imprimirLista(); //imprime la lista
+        lock.unlock();
+    }
+
+    public Integer getPar()throws InterruptedException{
+            lock.lock();
+            if (coleccion.isEmpty()) {
+                hayPar.await();
+                hayImpar.signal();
+                return 0;
+            }
+                if (coleccion.get(coleccion.size()-1) % 2 == 0) { //Mira si es par
+                    int x = coleccion.remove(coleccion.size()-1);
+                    hayHueco.signal();
+                    hayImpar.signal();
+                    imprimirLista();
+                    lock.unlock();
+                    return x;
+
+                }else{
+                    hayPar.await();
+                    lock.unlock();
+                    return 0;
+                }
 
     }
 
-    public synchronized Integer getPar()throws InterruptedException{
-
-    }
-
-    public synchronized Integer getImpar()throws InterruptedException{
+    public Integer getImpar()throws InterruptedException{
+            lock.lock();
+            if (coleccion.isEmpty()) {
+                hayImpar.await();
+                return 0;
+            }
+                if (coleccion.get(coleccion.size()-1) % 2 == 1) { //Mira si es impar
+                    int x = coleccion.remove(coleccion.size()-1);
+                    hayHueco.signal();
+                    hayPar.signal();
+                    imprimirLista();
+                    lock.unlock();
+                    return x;
+                }else{
+                    hayImpar.await();
+                    lock.unlock();
+                    return 0;
+                }
 
     }
 
